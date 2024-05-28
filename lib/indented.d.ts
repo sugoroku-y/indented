4522a785bafb67fd38108f4edf13b9c054ff67d8
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-05-29 00:23:19 +0900

    fix: ビルドが正しく動いていなかったので修正

--- lib/indented.d.ts
+++ lib/indented.d.ts
@@ -1,1 +1,11 @@
+/**
+ * インデント付きのテンプレートリテラルからインデントを除去した文字列を生成します。
+ * @param _ タグ付きテンプレートリテラルに指定されたテンプレート
+ * @param _.raw エスケープシーケンスを解除する前のテンプレート
+ * @param values タグ付きテンプレートリテラルに指定されたパラメーター
+ * @returns 生成した文字列を返します。
+ *
+ * 不正なエスケープシーケンスは`\`を除去するだけでエラーや例外にはなりません。
+ * ※ただしconsole.errorで出力します
+ */
 export declare function indented({ raw }: TemplateStringsArray, ...values: unknown[]): string;
--- lib/unescape.d.ts
+++ lib/unescape.d.ts
@@ -1,1 +1,6 @@
+/**
+ * エスケープシーケンスを解除した文字列を返します。
+ * @param s エスケープシーケンス付きの文字列
+ * @returns エスケープシーケンスを解除した文字列
+ */
 export declare function unescape(s: string): string;
